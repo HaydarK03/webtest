45896f0c96086bd6accb619f51f17d2b205d33fb
lab10 <kamilhaydar03@gmail.com>
2023-08-12 02:19:37 -0400

    add js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.navbar #menu');
+const nav = document.querySelector('.navbar .navbar-nav');
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('active')
+});
+
+
+
+
+
+
+
